bb41509f60f4e9cef1465af2493a735f4a8ba618
highjump <lixiang901112@163.com>
2018-06-18 11:33:07 +0800

    Added map to view

--- SweptUp/ViewController/LocationViewController.swift
+++ SweptUp/ViewController/LocationViewController.swift
@@ -11,10 +11,11 @@ import GoogleMaps
 
 class LocationViewController: BaseViewController {
 
+    @IBOutlet weak var mViewMap: GMSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadView()
+        initMap()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,19 +28,20 @@ class LocationViewController: BaseViewController {
         self.tabBarController?.navigationItem.title = "Location"
     }
     
-    override func loadView() {
+    func initMap() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 16.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-        
+//        let mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+//        view.addSubview(mapView)
+        mViewMap.camera = camera
+
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         marker.title = "Sydney"
         marker.snippet = "Australia"
-        marker.map = mapView
+        marker.map = mViewMap
     }
     
 
